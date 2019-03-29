f230904ff6ff8f592045bbff774079a35d8cd402
Luka <lukam@nym.hush.com>
2019-03-29 23:42:07 +0100

    Markdown Previewer
    Markdown Previewer for fCC Front-end libraries project.
    
    Deleted generic React comments and unneeded items. Started building the framework, and added onChange logic.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// insta update iz Textarea v Previewer: handleInput -> state.input: e.target.input
+// GIT markdown https://cdnjs.cloudflare.com/ajax/libs/marked/0.6.1/marked.js
+// on load componentDidMount(): a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+// on load render v HTML
+
+// opcijski bonus:
+// opcijski bonus: odpira linke v preview-u
+// carriage returns?? render as <br>
+
+
+export class Textarea extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+        <div className="editor-title"><h3 className="text-white"><i className="fas fa-code"></i> Editor</h3></div>
+        <textarea 
+          className="textarea form-control"
+          rows="10" 
+          id="editor" 
+          name="textarea" 
+          onChange={this.props.handleInput} 
+          defaultValue={this.props.markdown}></textarea>
+        </div>    
+      </div>
+    )
+  }
+}
+
+ function Previewer(props) {
+  return (
+    <div className="previewer container" id="preview">
+      <div className="previewer-title"><h3 className="text-white"><i className="far fa-eye"></i> Previewer</h3></div>
+      <div>
+      {props.preview}
+      {console.log(props.preview)}
+      {console.log('wtf!?')}
+      </div>
+    </div>
+  );
+}
+
+class App extends Component {
+    state = {
+      markdown: `e?`,
+      preview: `e?`
+        } 
+handleInput = (e) => {
+  console.log('handle input...');
+  this.setState({
+    markdown: e.target.value,
+    preview: e.target.value
+  })
+  console.log(this.state.preview);
+
+}
+componentDidMount() {
+  this.setState({
+    markdown: `e?`,
+    preview: `e?`
+  })
+}
+  
+  render() {    
+    return (
+      <div className="App wrapper">
+        <Textarea handleInput={this.handleInput} markdown={this.state.markdown}/>
+        <Previewer preview={this.state.preview}/>
       </div>
     );
   }
